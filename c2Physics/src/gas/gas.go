020accf843d7f1146dc96ae0aec0dd04e19e79e7
Sebastiano Rebonato-Scott <141144328+all-c-a-p-s@users.noreply.github.com>
2024-01-29 19:38:46 +0000

    Add files via upload

---
+++ c2Physics/src/gas/gas.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/draw"
+	"image/gif"
+	"math"
+	"math/rand"
+	"os"
+
+	"github.com/fogleman/gg"
+)
+
+type Coordinate struct {
+	x float64
+	y float64
+}
+
+type Velocity struct {
+	x float64
+	y float64
+}
+
+type Particle struct {
+	coords   Coordinate
+	velocity Velocity
+}
+
+func genParticles(n int, temp float64) []Particle {
+	// where n is number of particles to generate
+	k := temp / 10
+	particles := []Particle{}
+	for i := 0; i < n; i++ {
+		// TODO: check that particles are not generated in the same position
+		cX := rand.Float64() * 1000.0
+		cY := rand.Float64() * 1000.0
+		c := Coordinate{cX, cY}
+		vX := rand.Float64()*k - k/2
+		vY := rand.Float64()*k - k/2
+		v := Velocity{vX, vY}
+		newParticle := Particle{c, v}
+		particles = append(particles, newParticle)
+	}
+	return particles
+}
+
+func distance(p1, p2 Particle) float64 {
+	deltaX := p1.coords.x - p2.coords.x
+	deltaY := p1.coords.y - p2.coords.y
+	return math.Sqrt(deltaX*deltaX + deltaY*deltaY)
+}
+
+func colliding(p1, p2 Particle, radius float64) bool {
+	return distance(p1, p2) < radius*2
+}
+
+func updateParticles(particles []Particle) []Particle {
+	newParticles := particles
+	for i := range newParticles {
+		if newParticles[i].coords.y-20.0+newParticles[i].velocity.y < 0 {
+			newParticles[i].coords.y = 20
+			newParticles[i].velocity.y *= -1
+		} else if newParticles[i].coords.y+20.0+newParticles[i].velocity.y > 1000 {
+			newParticles[i].coords.y = 980
+			newParticles[i].velocity.y *= -1
+		} else if newParticles[i].coords.x-20.0+newParticles[i].velocity.x < 0 {
+			newParticles[i].coords.x = 20
+			newParticles[i].velocity.x *= -1
+		} else if newParticles[i].coords.x+20.0+newParticles[i].velocity.x > 1000 {
+			newParticles[i].coords.x = 980
+			newParticles[i].velocity.x *= -1
+		}
+		newParticles[i].coords.x += newParticles[i].velocity.x
+		newParticles[i].coords.y += newParticles[i].velocity.y
+		for j := range newParticles {
+			if j == i {
+				continue
+			}
+			if colliding(newParticles[i], newParticles[j], 20.0) {
+				tempX := newParticles[i].velocity.x
+				tempY := newParticles[i].velocity.y
+
+				newParticles[i].velocity.x = newParticles[j].velocity.x
+				newParticles[j].velocity.x = tempX
+
+				newParticles[i].velocity.y = newParticles[j].velocity.y
+				newParticles[j].velocity.y = tempY
+			}
+		}
+	}
+	return newParticles
+}
+
+func main() {
+	iterations := 500
+
+	var images []*image.Paletted
+	var delays []int
+	var disposals []byte
+
+	palette := color.Palette{
+		image.Transparent,
+		image.Black,
+		image.White,
+		color.RGBA{0xEE, 0xEE, 0xEE, 255},
+		color.RGBA{0xCC, 0xCC, 0xCC, 255},
+		color.RGBA{0x99, 0x99, 0x99, 255},
+		color.RGBA{0x66, 0x66, 0x66, 255},
+		color.RGBA{0x33, 0x33, 0x33, 255},
+	}
+
+	particles := genParticles(20, 500.0)
+	for i := 0; i < iterations; i++ {
+		dc := gg.NewContext(1000.0, 1000.0)
+		dc.SetRGBA(1, 1, 1, 0)
+		dc.Clear()
+		for _, c := range particles {
+			dc.DrawCircle(c.coords.x, c.coords.y, 20.0)
+		}
+
+		dc.SetRGBA(0, 0, 0, 1)
+		dc.Fill()
+
+		img := dc.Image()
+		bounds := img.Bounds()
+		dst := image.NewPaletted(bounds, palette)
+		draw.Draw(dst, bounds, img, bounds.Min, draw.Src)
+		images = append(images, dst)
+		delays = append(delays, 2)
+		disposals = append(disposals, gif.DisposalBackground)
+
+		particles = updateParticles(particles)
+	}
+	file, err := os.OpenFile("gas.gif", os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		panic("error creating file")
+	}
+	defer file.Close()
+	gif.EncodeAll(file, &gif.GIF{
+		Image:    images,
+		Delay:    delays,
+		Disposal: disposals,
+	})
+}
---
+++ c2Physics/src/solid/solid.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/draw"
+	"image/gif"
+	"math"
+	"math/rand"
+	"os"
+
+	"github.com/fogleman/gg"
+)
+
+var START_CENTRES []Coordinate = startCentres()
+
+type Coordinate struct {
+	x float64
+	y float64
+}
+
+func distance(x, y float64) float64 {
+	return math.Sqrt(x*x + y*y)
+}
+
+func startCentres() []Coordinate {
+	var centres []Coordinate
+	for i := 200; i <= 800; i += 50 {
+		for j := 200; j <= 800; j += 50 {
+			c := Coordinate{
+				x: float64(i),
+				y: float64(j),
+			}
+			centres = append(centres, c)
+		}
+	}
+	return centres
+}
+
+func UpdateCentres(centres []Coordinate, temp float64) []Coordinate {
+	k := temp / 100.0
+	newCentres := centres
+	for i := range newCentres {
+		newCentres[i].x += rand.Float64()*k - k/2
+		newCentres[i].y += rand.Float64()*k - k/2
+		resetCount := 0
+		for distance(newCentres[i].x-START_CENTRES[i].x, newCentres[i].y-START_CENTRES[i].y) > 5.0 {
+			resetCount++
+			newCentres[i].x = START_CENTRES[i].x
+			newCentres[i].y = START_CENTRES[i].y
+			if resetCount >= 3 {
+				break
+			}
+			newCentres[i].x += rand.Float64()*k - k/2
+			newCentres[i].y += rand.Float64()*k - k/2
+
+		}
+	}
+	return newCentres
+}
+
+func main() {
+	iterations := 100
+	centres := startCentres()
+
+	var images []*image.Paletted
+	var delays []int
+	var disposals []byte
+
+	palette := color.Palette{
+		image.Transparent,
+		image.Black,
+		image.White,
+		color.RGBA{0xEE, 0xEE, 0xEE, 255},
+		color.RGBA{0xCC, 0xCC, 0xCC, 255},
+		color.RGBA{0x99, 0x99, 0x99, 255},
+		color.RGBA{0x66, 0x66, 0x66, 255},
+		color.RGBA{0x33, 0x33, 0x33, 255},
+	}
+
+	for i := 0; i < iterations; i++ {
+		dc := gg.NewContext(1000.0, 1000.0)
+		dc.SetRGBA(1, 1, 1, 0)
+		dc.Clear()
+		for _, c := range centres {
+			dc.DrawCircle(c.x, c.y, 20.0)
+		}
+
+		dc.SetRGBA(0, 0, 0, 1)
+		dc.Fill()
+
+		img := dc.Image()
+		bounds := img.Bounds()
+		dst := image.NewPaletted(bounds, palette)
+		draw.Draw(dst, bounds, img, bounds.Min, draw.Src)
+		images = append(images, dst)
+		delays = append(delays, 10)
+		disposals = append(disposals, gif.DisposalBackground)
+
+		centres = UpdateCentres(centres, 1000.0)
+	}
+
+	file, err := os.OpenFile("solid.gif", os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		panic("error creating file")
+	}
+	defer file.Close()
+	gif.EncodeAll(file, &gif.GIF{
+		Image:    images,
+		Delay:    delays,
+		Disposal: disposals,
+	})
+}
